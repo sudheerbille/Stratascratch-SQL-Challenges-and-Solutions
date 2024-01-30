30f94792af7a503cc08056899b53a6199ceb3615
sudheerbille <148851067+sudheerbille@users.noreply.github.com>
2024-01-30 23:51:18 +0530

    Update ID 10315 - Cities With The Most Expensive Homes.sql

--- Medium/ID 10315 - Cities With The Most Expensive Homes.sql
+++ Medium/ID 10315 - Cities With The Most Expensive Homes.sql
@@ -3,15 +3,6 @@
 Question:
 Write a query that identifies cities with higher than average home prices when compared to the national average. Output the city names.
 
-Table: zillow_transactions
-
-Table Structure:
-id: int
-state: varchar
-city: varchar
-street_address: varchar
-mkt_price: int
-
 Solution:
 WITH CTE AS 
 (SELECT CITY, 
@@ -20,5 +11,4 @@ FROM zillow_transactions
 GROUP BY 1
 HAVING AVG(MKT_PRICE) > (SELECT AVG(MKT_PRICE) FROM zillow_transactions)
 )
-
 SELECT CITY FROM CTE;
