7f92e2f8163d9723889ab91d571175b94d9c0f9d
sudheerbille <148851067+sudheerbille@users.noreply.github.com>
2024-01-30 23:53:19 +0530

    Update ID 10013 - Positive Ad Channels.sql

--- Hard/ID 10013 - Positive Ad Channels.sql
+++ Hard/ID 10013 - Positive Ad Channels.sql
@@ -1,19 +1,7 @@
-## https://platform.stratascratch.com/coding/10013-positive-ad-channels?code_type=3 ##
-
 ## ID 10013 - Positive Ad Channels ##
 
 Question:
-
 Find the advertising channel with the smallest maximum yearly spending that still brings in more than 1500 customers each year.
-
-Table: uber_advertising
-
-Table Structure:
-
-year: int
-advertising_channel: varchar
-money_spent: int
-customers_acquired: int
 
 Solution:
 
