ef78848785976fe32d637d5a1631858232ee8547
sudheerbille <148851067+sudheerbille@users.noreply.github.com>
2024-02-03 10:25:26 +0530

    Update ID 10000 - Find the year that Uber acquired more than 2000 customers through celebrities.sql

--- Easy/ID 10000 - Find the year that Uber acquired more than 2000 customers through celebrities.sql
+++ Easy/ID 10000 - Find the year that Uber acquired more than 2000 customers through celebrities.sql
@@ -1,18 +1,7 @@
-## https://platform.stratascratch.com/coding/10000-find-the-year-that-uber-acquired-more-than-2000-customers-through-celebrities?code_type=3 ##
-
 ## ID 10000 - Find the year that Uber acquired more than 2000 customers through celebrities ##
 
 Question:
 Find the year that Uber acquired more than 2000 customers through advertising using celebrities.
-
-Table: uber_advertising
-
-Table Structure:
-
-year: int
-advertising_channel: varchar
-money_spent: int
-customers_acquired: int
 
 Solution:
 
