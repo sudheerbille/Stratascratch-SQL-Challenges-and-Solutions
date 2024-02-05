cd88b99f64c5e18a381c128e7385615b7773d503
sudheerbille <148851067+sudheerbille@users.noreply.github.com>
2024-02-05 23:31:07 +0530

    Update ID 9892 - Second Highest Salary.sql

--- Medium/ID 9892 - Second Highest Salary.sql
+++ Medium/ID 9892 - Second Highest Salary.sql
@@ -4,6 +4,18 @@ Find the second highest salary of employees.
 
 Solution:
 
-SELECT SALARY FROM employee
+SELECT SALARY 
+FROM employee
 ORDER BY SALARY DESC
 LIMIT 1 OFFSET 1;
+
+OR
+
+WITH CTE AS(
+SELECT SALARY,
+       DENSE_RANK()OVER(ORDER BY SALARY DESC) AS RN
+FROM employee
+)
+SELECT SALARY 
+FROM CTE 
+WHERE RN IN (2);
